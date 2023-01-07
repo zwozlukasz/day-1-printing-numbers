6408c2d299009988014c9f613728c8f72a438b05
Łukasz <109048586+zwozlukasz@users.noreply.github.com>
2023-01-07 02:45:40 +0000

    How to display string with int and float values

--- main.py
+++ main.py
@@ -1,44 +1,26 @@
-# repeat the same text several times
-# first way
-print('My text')
-print('My text')
-print('My text')
-print('\n')
+# How to printing numbers:
+value_1 = 3442
+value_2 = 24
 
-# second way - we can use """ text """ or ''' text '''
-print('''My text
-My text
-My text''')
-print()
+# 1
+print(f"Show values: value_1 = {value_1:6}, value_2 = {value_2:6}")
 
-# third way
-print("My text \nMy text \nMy text\n")
+# 2
+print("Show values: value_1 = %6d, value_2 = %6d"%(value_1, value_2))
 
-# fourth way
-print("My text\n"*3)
+# 3
+print("Show values: value_1 = {0:6}, value_2 = {1:6}".format(value_1, value_2))
 
-message = "This is test message"
-message_2 = "Second message"
-print(f"Show message: {message}. Next message: {message_2}")
+# float values
+value_1 = 453.524434434
+value_2 = 2.423424242
+value_3 = 6.32
 
-print("Hello", "Jenny")
-# but 
-print("Hello"+"Jenny")
-# extended case 1
-print("Hello" "Jenny" "Margaret")
-# but 
-print("Hello", "Jenny", "Margaret")
-print()
+# 1
+print(f"Resutls: value_1: {value_1:4.3}, value_2: {value_2:4.5}, value_3: {value_3:4.3}")
 
-# format stings
-# 1.
-value_1 = 'example 1'
-value_2 = 'example 2'
-value_3 = 'example 3'
+# 2
+print("results: %.3f, %.3f, %.3f"%(value_1, value_2, value_3))
 
-result = "Value 1 is: %s value 2 is %s value 3 is %s"%(value_1, value_2, value_3)
-
-print(result)
-#2
-result = "This is example no 1: {}, example no 2: {}, example no 3: {}".format(value_1, value_2, value_3)
-print(result)
+# 3
+print("results: value_1: {:.3} value_2: {:.3} value_3: {:.3}".format(value_1, value_2, value_3))
